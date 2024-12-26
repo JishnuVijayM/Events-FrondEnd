4b78a86b5744e71836338bccde89456d4e7a4447
JishnuVijayM <jishnupmd2002@gmail.com>
2024-12-26 11:37:52 +0530

    Auth confiq

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-cover bg-center w-auto bg-black">
+            <div className="w-[84rem] h-[34rem] bg-violet-300 shadow-lg flex">
+                <div className="flex-1 bg-gradient-to-b from-white to-primary w-full h-full flex direction-column">Div 1
+{/* 
+                    <h1>dss</h1>
+                    <p>sd</p> */}
+                </div>
+
+
+                <div className="flex-1 bg-white w-full h-ful ">Div 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
